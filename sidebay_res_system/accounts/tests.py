5cf8cb6044f2217d2b997d57f044ac24ed442258
ringoshogo <30945996+ringoshogo@users.noreply.github.com>
2019-03-31 15:45:37 +0900

    Hayashi_add
    Hayashi_add

--- sidebay_res_system/accounts/tests.py
+++ sidebay_res_system/accounts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from selenium import webdriver
+import unittest
+from django.urls import resolve
+from .views import login
+from django.http import HttpRequest
 
-# Create your tests here.
+
+class loginTest(unittest.TestCase):
+    def setUp(self):
+        self.blowser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.blowser.quit()
+
+    def root_urls_resolve_to_login_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, login)
+
+
+    def fill_ipout_form(self):
+        self.blowser.get('http://localhost:8000')
+
+        self.assertIn('Sidebay予約システム', self.blowser.title)
+        self.fail('Finish the Test!!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
