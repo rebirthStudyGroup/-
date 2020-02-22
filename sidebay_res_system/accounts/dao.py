81587f462b8b274d2649c419e3c7eb4085085a08
rebirth-adimin <30945996+ringoshogo@users.noreply.github.com>
2020-02-22 17:10:36 +0900

    #24 対応
    施設利用不可日の登録を

--- sidebay_res_system/accounts/dao.py
+++ sidebay_res_system/accounts/dao.py
@@ -1,8 +1,11 @@
 from datetime import date, timedelta
 from django.db import connection, transaction
+from django.db.utils import IntegrityError
 
-TABLE = "calendar_master"
-NG_COL = "ng_date"
+CAL_TABLE = 'calendar_master'
+NUM_TABLE = "numbering"
+NG_COL = 'ng_date'
+RES_COL = "reservation_id"
 
 class CalendarMaster:
     """施設不可日登録情報を操作するクラス"""
@@ -10,9 +13,8 @@ class CalendarMaster:
     @staticmethod
     def get_ngdate() -> list:
         """施設利用不可日を取得する"""
-        today = date.today().strftime("%Y-%m-%d")
         with connection.cursor() as cursor:
-            cursor.execute("select {ng_column} from {table} where ng_date >= '{today}'".format(ng_column=NG_COL, table=TABLE, today=today))
+            cursor.execute("select ng_date from calendar_master where ng_date >= %s", [date.today().strftime("%Y-%m-%d")])
             result = cursor.fetchall()
             return [x[0] for x in result]
 
@@ -21,17 +23,23 @@ class CalendarMaster:
         """引数で受け取った月度の施設利用不可日を取得する"""
         today = date.today().strftime("%Y-%m-%d")
         with connection.cursor() as cursor:
-            cursor.execute("select {ng_column} from {table} where (DATE_FORMAT({ng_column}, '%Y%m') = '{Y}{m}')".format(ng_column=NG_COL, table=TABLE, Y=str(year), m=str(month).zfill(2)))
+            cursor.execute("select ng_date from calendar_master where (DATE_FORMAT(ng_date, %s) = %s)", ['%Y%m', str(year) + str(month).zfill(2)])
             result = cursor.fetchall()
             return [x[0] for x in result]
 
-
     @staticmethod
-    def set_ngdate(ng_date: date, reason:str):
+    def set_ngdate(ng_date: date, reason:str) -> str:
         """施設利用不可日を設定する"""
+
         with connection.cursor() as cursor:
-            cursor.execute("insert into {table} values('{ng_date}', '{reason}')".format(table=TABLE, ng_date=ng_date.strftime("%Y-%m-%d"), reason=reason))
-            transaction.commit()
+            try:
+                print(ng_date.strftime("%Y-%m-%d"))
+                cursor.execute("insert into calendar_master values(%s, %s)", [ng_date.strftime("%Y-%m-%d"), reason])
+                transaction.commit()
+            except IntegrityError:
+                return "既に登録されております"
+        return ""
+
 
     @staticmethod
     def is_in_ngdate(start_date: date, end_date: date):
@@ -41,6 +49,26 @@ class CalendarMaster:
             if start_date + timedelta(days=day) in ng_days:
                 return True
         return False
+
+class NumberingManagement:
+    """採番テーブル"""
+
+    @staticmethod
+    def get_num():
+        """採番テーブルの予約IDを更新（＋1）し、最新値を取得する"""
+        with connection.cursor() as cursor:
+            cursor.execute("update {table} t1 inner join (select {column} from {table} for update)t2 set t1.{column} = t2.{column} + 1".format(table=NUM_TABLE, column=RES_COL))
+
+    @staticmethod
+    def _get_num():
+        """採番テーブルから予約IDを取得する"""
+        with connection.cursor() as cursor:
+            cursor.execute("select reservation_id from numbering")
+            cursor.execute("update {table} t1 inner join (select {column} from {table} for update)t2 set t1.{column} = t2.{column} + 1 where current of current".format(table=NUM_TABLE, column=RES_COL))
+            return cursor.fetchone()
+
+
+
 
 
 
