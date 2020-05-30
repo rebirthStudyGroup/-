59f116a3d94f2502a3850041cf6d321f0ec0186c
akatsu-h <akatsu.h@re-birth-it.jp>
2020-05-30 12:51:13 +0900

    セッションタイムアウト後にログアウトするとStackTrace画面が表示される不具合を修正

--- sidebay_res_system/accounts/views.py
+++ sidebay_res_system/accounts/views.py
@@ -306,6 +306,9 @@ def confirm_res_app(request):
 
 
 def logout_user(request):
+    if not __is_login_user(request):
+      return TemplateResponse(request, URL_REBGST001, {"error": ""})
+
     del request.session[ADMIN_FLG]
     del request.session[LOG_USR]
     del request.session[LOG_NAME]
