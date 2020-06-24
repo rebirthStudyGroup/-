cd952d74bc0729aa93957786a59409c72803f722
akatsu-h <akatsu.h@re-birth-it.jp>
2020-06-24 21:06:30 +0900

    DBのrootのパスワードを変更

--- sidebay_res_system/res_system/settings_prod.py
+++ sidebay_res_system/res_system/settings_prod.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'res_system',
         'USER': 'root',
-        'PASSWORD': 'Password',
+        'PASSWORD': 'BVvL$Fv/q1a8',
         'HOST': 'yoyakukun_db',
         'PORT': '3306',
         'OPTIONS': {
